dae73cf37ad8cc011b0a3e762162c0e288f15f7f
KingKebs <kebogile1201@icloud.com>
2020-07-07 12:23:47 +0200

    user schema mongodb
    Connected to Mongodb Atlas successfully
    creating a user database schema

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 // creating the backend and connecting to MongoDB atlas
-
+// Dependancies
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -9,9 +9,11 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+// CORS middleware - for parsing out JSON
 app.use(cors());
 app.use(express.json());
 
+// CONNECTING TO MONGODB ATLAS SERVER
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const connection = mongoose.connection;
@@ -19,11 +21,11 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-const exercisesRouter = require("./routes/exercises");
-const usersRouter = require("./routes/users");
+// const exercisesRouter = require("./routes/exercises");
+// const usersRouter = require("./routes/users");
 
-app.use("/exercises", exercisesRouter);
-app.use("/users", usersRouter);
+// app.use("/exercises", exercisesRouter);
+// app.use("/users", usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);

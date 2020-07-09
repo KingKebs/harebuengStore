ffe6044f56d1d1444a561b9cc1d7b975b913deb6
KingKebs <kebogile1201@icloud.com>
2020-07-10 01:36:36 +0200

    frontend
    frontend initialisations
    app.js deps and structure

--- backend/Routes/users.js
+++ backend/Routes/users.js
@@ -1,12 +1,15 @@
+//
 const router = require("express").Router();
 let User = require("../models/user.model");
 
+// 1st router endpoint which serves http request
 router.route("/").get((req, res) => {
   User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+// this router serves POST request
 router.route("/add").post((req, res) => {
   const username = req.body.username;
 
